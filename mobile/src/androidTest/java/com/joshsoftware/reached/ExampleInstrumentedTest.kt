e2dd64e172d189883617dcf54df1d34df7928e83
Nayan <ndhabarde11@gmail.com>
2021-04-06 09:42:26 +0530

    Migrated to new firebase project

--- mobile/src/androidTest/java/com/joshsoftware/reached/ExampleInstrumentedTest.kt
+++ mobile/src/androidTest/java/com/joshsoftware/reached/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.joshsoftware.fampal", appContext.packageName)
+        assertEquals("com.joshsoftware.reached", appContext.packageName)
     }
 }
