381cb225aa404b15ae541107676b1f598bde46c9
RishabhS05 <rishabhshrivastava.27@gmail.com>
2021-07-06 20:56:01 +0530

    Remove delete option for group admin.

--- mobile/src/main/java/com/joshsoftware/reached/ui/adapter/MemberEditAdapter.kt
+++ mobile/src/main/java/com/joshsoftware/reached/ui/adapter/MemberEditAdapter.kt
@@ -1,6 +1,7 @@
 package com.joshsoftware.reached.ui.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.joshsoftware.core.model.Member
@@ -8,15 +9,21 @@ import com.joshsoftware.reached.R
 import com.joshsoftware.reached.ui.viewholder.ViewHolder
 import kotlinx.android.synthetic.main.edit_member_view.view.*
 
-class MemberEditAdapter(val onClick: (Member, Int) -> Unit): ListAdapter<Member, ViewHolder>(MemberAdapter.DIFF_CALLBACK) {
+class MemberEditAdapter(val onClick: (Member, Int) -> Unit, val groupId: String) :
+    ListAdapter<Member, ViewHolder>(MemberAdapter.DIFF_CALLBACK) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.edit_member_view, parent, false))
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.edit_member_view, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val model = getItem(position)
         if(model != null) {
             holder.itemView.apply {
+                if(groupId == model.id)
+                    imgDelete.visibility = View.GONE
+
                 imgDelete.setOnClickListener {
                     onClick(model, position)
                 }
@@ -24,6 +31,4 @@ class MemberEditAdapter(val onClick: (Member, Int) -> Unit): ListAdapter<Member,
             }
         }
     }
-
-
 }
